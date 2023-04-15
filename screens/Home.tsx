7f6704dde4ca0e031632b07b3aa670da4b148940
TheRealDeath <gv418239@g.risd.org>
2023-04-14 22:34:30 -0700

    fixed png to jpg, added functionality

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <ImageBackground
-      source={require("../assets/images/bg.png")}
+      source={require("/Users/bsidesdfw/Desktop/tinder-expo/assets/images/bg.png")}
       style={styles.bg}
     >
       <View style={styles.containerHome}>
